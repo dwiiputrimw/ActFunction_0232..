1210a45fae3e5e7eb9d1811fe0d0ea3d576980fd
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:07:29 +0700

    Menambahkan struktur dasar program dan deklarasi variabel

---
+++ challenge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+float Luas, d1, d2;
+
+void ProsedurInputData(){
+    cout << "Masukkan Diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan Diagonal 2 : ";
+    cin >> d2;
+}
