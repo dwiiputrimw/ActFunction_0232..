bceb5d3e4f0cf0bfeb37ce782aadc9f0a61e61ac
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:10:58 +0700

    Tambah prosedur input dan fungsi hitung luas

--- challenge.cpp
+++ challenge.cpp
@@ -9,3 +9,12 @@ void ProsedurInputData(){
     cout << "Masukkan Diagonal 2 : ";
     cin >> d2;
 }
+
+float ProsedurHitungLuasBerparameter(float d1, float d2) 
+{
+    return 0.5 * d1 * d2;
+}
+
+void ProsedurOutput (){
+    cout << "Luas Belah Ketupat = " << ProsedurHitungLuasBerparameter(d1, d2);
+}
