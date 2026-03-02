aa9802bf317ac57b271316159f12e0a959bf29a5
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:20:18 +0700

    Menambahkan fungsi perhitungan luas persegi panjang

--- parameter1.cpp
+++ parameter1.cpp
@@ -1,12 +1,1 @@
-//library
-#include <iostream>
-using namespace std;
-//deklarasi variabel global
-int panjang, lebar;
-//implementasi fungsi dan prosedur
-void input(){
-    cout << "masukkan panjang : ";
-    cin >> panjang;
-    cout << "masukkan lebar : ";
-    cin >> lebar;
-}
+
---
+++ parameter3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//deklarasi variabel global
+int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "masukkan panjang : ";
+    cin >> panjang;
+    cout << "masukkan lebar : ";
+    cin >> lebar;
+}
+int luaspersegi( int a,int b){
+    return a * b;
+}
+int jumlah(int x, int y, int z){
+    return x+y+z;
+}
+void output(){
+    cout << "Hasilnya : " << luaspersegi(panjang,lebar) << endl; 
+}
