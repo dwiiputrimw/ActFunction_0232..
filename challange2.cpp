ed4902d3deaf19cbff2ecdadb37bdc36b7464454
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:14:06 +0700

    Tambah prosedur output dan perbaikan tampilan hasil

---
+++ challange2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+float Luas, d1, d2;
+
+void ProsedurInputData(){
+    cout << "Masukkan Diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan Diagonal 2 : ";
+    cin >> d2;
+}
+
+
+float ProsedurHitungLuasBerparameter(float d1, float d2) 
+{
+    return 0.5 * d1 * d2;
+}
+
+void ProsedurOutput (){
+    cout << "Luas Belah Ketupat = " << ProsedurHitungLuasBerparameter(d1, d2);
+}
+   
+
+int main () {
+    ProsedurInputData();
+    ProsedurOutput();   
+
+}
