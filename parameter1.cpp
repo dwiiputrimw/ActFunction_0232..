41916a2fb80dda0e8321ad7b5f355a2776d5de92
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:16:50 +0700

    Membuat struktur dasar program dan deklarasi variabel global

---
+++ parameter1.cpp
@@ -0,0 +1,12 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel global
+int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "masukkan panjang : ";
+    cin >> panjang;
+    cout << "masukkan lebar : ";
+    cin >> lebar;
+}
