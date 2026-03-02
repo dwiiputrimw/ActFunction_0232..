432352141d817118d952b103f23efbd3110e0ab2
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-02 22:26:33 +0700

    Menambahkan prosedur output untuk menampilkan hasil luas

---
+++ fungsi3.cpp
@@ -0,0 +1,22 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel
+int p, l;
+//implementasi fungsi dan prosedur
+void input (){
+    cout << "masukkan panjang : ";
+    cin >> p;
+    cout << "masukkan lebar : ";
+    cin >> l;
+}
+int Luaspersegi(){
+    return p * l;
+}
+void output(){
+    cout << "Hasilnya : " << luaspersegi();
+}
+int main(){ //mulai
+    input();
+    output();
+}//selesai
